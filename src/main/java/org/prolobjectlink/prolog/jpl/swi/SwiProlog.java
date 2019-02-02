fc89e6a7f5867cd3007176541ff77ac4868e6957
Jose Zalacain <Jose Zalacain@10.11.0.25>
2019-02-02 11:21:08 -0400

    Replace WorkLogic by Prolobjectlink

---
+++ src/main/java/org/prolobjectlink/prolog/jpl/swi/SwiProlog.java
@@ -0,0 +1,48 @@
+/*
+ * #%L
+ * prolobjectlink-jpi-jpl-swi
+ * %%
+ * Copyright (C) 2019 Prolobjectlink Project
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package org.prolobjectlink.prolog.jpl.swi;
+
+import org.prolobjectlink.prolog.PrologConverter;
+import org.prolobjectlink.prolog.PrologEngine;
+import org.prolobjectlink.prolog.PrologProvider;
+import org.prolobjectlink.prolog.jpl.JplProvider;
+
+import jpl.Term;
+
+public class SwiProlog extends JplProvider implements PrologProvider {
+
+	public SwiProlog() {
+		super(new SwiPrologConverter());
+	}
+
+	public SwiProlog(PrologConverter<Term> converter) {
+		super(converter);
+	}
+
+	public PrologEngine newEngine() {
+		return new SwiPrologEngine(this);
+	}
+
+	@Override
+	public String toString() {
+		return "SwiPrologProvider [converter=" + converter + "]";
+	}
+
+}
